06290f672a6eec48dfec1ff0642c2757147f71c6
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-06-27 00:02:34 +0200

    Test

--- kube-deploy/scripts/CheckBlue.sh
+++ kube-deploy/scripts/CheckBlue.sh
@@ -7,10 +7,10 @@ do
     echo "Wait 2 seconds .."
     sleep 2
     kubectl get deploy sampleapp-deployment-TARGET_TAG
-    STATUS=$(kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "1/1" | wc -l)
-    READY="1"
+    STATUS=`kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "1/1" | wc -l`
+    READY=1
 
-    if [ "$STATUS" == "$READY" ]
+    if [ $STATUS == $READY ]
     then
         break
     fi

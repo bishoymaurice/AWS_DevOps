226315a05a5f221e3069ffe4ff27c6d6c4221a57
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-06-26 23:31:35 +0200

    Test

--- kube-deploy/scripts/DestroyBlue.sh
+++ kube-deploy/scripts/DestroyBlue.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+kubectl get deployments | grep -v sampleapp-deployment-TARGET_TAG
+
 OLD_DEPLOYMENT=$(kubectl get deployments | grep -v sampleapp-deployment-TARGET_TAG | awk '{ print $1 }' | grep -v NAME)
-echo "Old deployment is: ${OLD_DEPLOYMENT} and will be destroyed"
-kubectl delete deployment ${OLD_DEPLOYMENT} || echo "Not found"
+
+echo "Old deployment is: $OLD_DEPLOYMENT and will be destroyed"
+
+kubectl delete deployment $OLD_DEPLOYMENT || echo "Not found"

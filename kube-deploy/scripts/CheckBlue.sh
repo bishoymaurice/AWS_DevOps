09a8b62a29e6b84f4bf0dac51733cf8772c967b0
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-06-27 00:08:42 +0200

    Test

--- kube-deploy/scripts/CheckBlue.sh
+++ kube-deploy/scripts/CheckBlue.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-a=0
-
-while [ $a -lt 10 ] 
+until [ `kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "1/1" | wc -l` -gt 0 ]
 do
     echo "Wait 2 seconds .."
     sleep 2
-    kubectl get deploy sampleapp-deployment-TARGET_TAG
-    STATUS=`kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "1/1" | wc -l`
-    READY=1
-
-    if [ $STATUS eq $READY ]
-    then
-        break
-    fi
-
 done

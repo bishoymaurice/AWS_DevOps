9225048cc4644f384c08ee323b3ce86c95fc4f06
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-06-27 18:48:01 +0200

    Blue green deployment

--- kube-deploy/scripts/CheckBlue.sh
+++ kube-deploy/scripts/CheckBlue.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-until [ `kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "1/1" | wc -l` -gt 0 ]
+# Check on 2/2 becuase we're spinning 2 replicas in app-deploy.yaml file
+
+until [ `kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "2/2" | wc -l` -gt 0 ]
 do
     echo "Wait 2 seconds .."
     sleep 2

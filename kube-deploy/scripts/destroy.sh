b7181f5fbb5c1f55bf728b613ea89ef5ec26e08b
Bishoy Maurice <bishoy.maurice@valeo.com>
2021-06-28 15:43:17 +0200

    Update deployment script

--- kube-deploy/scripts/destroy.sh
+++ kube-deploy/scripts/destroy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-kubectl get deployments | grep -v sampleapp-deployment-TARGET_TAG
+kubectl get deployments | grep -v sampleapp-deployment-nginx
 
-OLD_DEPLOYMENT=$(kubectl get deployments | grep -v sampleapp-deployment-TARGET_TAG | awk '{ print $1 }' | grep -v NAME)
+OLD_DEPLOYMENT=$(kubectl get deployments | grep -v sampleapp-deployment-nginx | awk '{ print $1 }' | grep -v NAME)
 
 echo 'Old deployment is: "$OLD_DEPLOYMENT" and will be destroyed'
 
--- kube-deploy/scripts/wait.sh
+++ kube-deploy/scripts/wait.sh
@@ -2,7 +2,7 @@
 
 # Check on 2/2 becuase we're spinning 2 replicas in app-deploy.yaml file
 
-until [ `kubectl get deploy sampleapp-deployment-TARGET_TAG | grep "2/2" | wc -l` -gt 0 ]
+until [ `kubectl get deploy sampleapp-deployment-nignx | grep "2/2" | wc -l` -gt 0 ]
 do
     echo "Wait 2 seconds .."
     sleep 2
